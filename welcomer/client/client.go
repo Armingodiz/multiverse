68a61d48a545132f331d9fc543b529731abba55b
Armin <armin.godarzi@utopiops.com>
2022-08-06 00:15:43 +0430

    unary call added to client

--- welcomer/client/client.go
+++ welcomer/client/client.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"multiverse/welcomer/welcomepb"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+type Client struct {
+	grpcClient welcomepb.WelcomeServiceClient
+}
 
 func main() {
 	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials())) // for now because we are dont have a certificate
@@ -14,6 +21,27 @@ func main() {
 		panic(err)
 	}
 	defer conn.Close()
-	client := welcomepb.NewWelcomeServiceClient(conn)
-	fmt.Println("client:", client)
+	cli := welcomepb.NewWelcomeServiceClient(conn)
+	client := &Client{cli}
+	response, err := client.Welcome(welcomepb.UserInfo{
+		Name:    "Armin",
+		Country: "Iran",
+		Age:     21,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("response:", response)
+}
+
+func (client *Client) Welcome(user welcomepb.UserInfo) (*welcomepb.WelcomeResponse, error) {
+	response, err := client.grpcClient.Welcome(context.Background(), &welcomepb.WelcomeRequest{
+		User: &welcomepb.UserInfo{
+			Name:    user.Name,
+			Country: user.Country,
+			Age:     user.Age,
+		},
+		Arrival: timestamppb.New(time.Now()),
+	})
+	return response, err
 }
