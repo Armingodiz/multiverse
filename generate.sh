fbce7f7cc7a9dc14d0834f3125af8a113ca11865
Armin <armin.godarzi@utopiops.com>
2022-08-11 01:01:11 +0430

    generate sh removed

--- generate.sh
+++
@@ -1,6 +0,0 @@
-protoc --go_out=welcomer/. --go-grpc_out=welcomer/. welcomer/welcomepb/welcome.proto
-go run welcomer/server/server.go
-go run welcomer/client/client.go
-protoc --go_out=calculator/. --go-grpc_out=calculator/. calculator/calculatorpb/calculator.proto
-go run calculator/server/server.go
-go run calculator/client/client.go
